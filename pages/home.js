3ac45d9ac6cb0df2e3d67937002e025b6bad7c2a
NicklanderM <157871815+NicklanderM@users.noreply.github.com>
2026-09-09 17:13:46 +0100

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -106,7 +106,7 @@ return`
                 <img
                     class="parallax"
                     data-speed="30"
-                  "assets/images/products/angel.png"
+                  <img src="/ad-lifestyle-v2/assets/products/angel.png">
                     alt="Angel Moon">
 
             </div>
