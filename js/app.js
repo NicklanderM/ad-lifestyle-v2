40475652da3fde95922b7ad75122186efdd50618
NicklanderM <157871815+NicklanderM@users.noreply.github.com>
2026-09-11 13:58:32 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -331,9 +331,19 @@ function createFooter(){
 
                         </a>
 
-                        <a href="https://wa.me/${number}?text=${encodeURIComponent(message)};">
+                        <a href="">
 
                             WhatsApp
+
+                        </a>
+                                                <a href="https://web.facebook.com/ad.ambassadoracademy21">
+
+                            Facebook
+
+                        </a>
+                                                <a href="https://www.instagram.com/ad.ambassadoracademy21?stkn=MXI2MmVmZDZwZzBhZw==">
+
+                            Instagram
 
                         </a>
                                             
