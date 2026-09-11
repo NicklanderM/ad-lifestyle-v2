328640418e2cdf5cd3ebfdf0347ccdbf62a6a3d3
NicklanderM <157871815+NicklanderM@users.noreply.github.com>
2026-09-11 14:34:01 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -13,7 +13,7 @@ import { loadHome } from "../pages/home.js";
 import { loadServices } from "../pages/services.js";
 import { loadEvents } from "../pages/events.js";
 import { loadContact } from "../pages/contact.js";
-import { loadContact } from "../pages/about.js";
+import { loadAbout } from "../pages/about.js";
 
 import { loadAngel } from "../pages/products/angel.js";
 import { loadEzeno } from "../pages/products/ezeno.js";
